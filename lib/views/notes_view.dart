efcf9d54d973f4123f72661136904d2b68f11475
Your Name <omranaboaliii@gmail.com>
2024-08-30 14:47:35 +0300

    create note app and  create notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
